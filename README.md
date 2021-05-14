8a3553e1540b732a883e47150f3ff67fcad38073
Vinícius Gonçalves de Oliveira <viniciusp.olivera@gmail.com>
2021-05-14 17:25:00 -0300

    Plugin format finished

--- README.md
+++ README.md
@@ -2,11 +2,33 @@
 
 A Neovim Linter for the École 42 Norme.
 
+> PS: The plugin is in development. Still not usable.
+
 Requirements
 ---
 
 - NeoVim `v0.5+`(nightly release)
 - [mfussenegger/nvim-lint](https://github.com/mfussenegger/nvim-lint)
+- Executable `norminette` in your PATH
+
+Configuration
+---
+
+In your `nvim-lint` configuration you just have to add:
+
+```lua
+local lint = require('lint')
+local norme = require('norme').linter
+
+lint.linters.norme = norme
+
+print(norme)
+
+require('lint').linters_by_ft = {
+	c = { 'norme', },
+	cpp = { 'norme', }, -- for header and C++ files
+}
+```
 
 Author
 ---
--- lua/norme.lua
+++ lua/norme.lua
@@ -1,13 +1,15 @@
+local M = {}
+
 local ERROR = vim.lsp.protocol.DiagnosticSeverity.Error
 local pattern = 'Error:%s+([%a+_?]+)%s+%(line:%s+(%d+),%s+col:%s+(%d+)%):%s+(.*)$'
 local diagnostic_skeleton = { source = 'norminette', severity = ERROR }
 
-local norminette_parser = function(output)
+local parser = function(output)
 	local result = vim.fn.split(output, "\n")
 	local diagnostics = {}
 
 	for idx, message in ipairs(result) do
-		if idx == 1 then goto continue end -- jump first line of norminette output
+		if idx == 1 then goto continue end -- Jump first line of Norminette output. E.g: "File.c: OK!"
 		local  code, lineno, offset, msg = string.match(message, pattern)
 		lineno = tonumber(lineno or 1) - 1
 		offset = tonumber(offset or 1) - 1
@@ -25,19 +27,13 @@ local norminette_parser = function(output)
 	return diagnostics
 end
 
-require('lint').linters.norme = {
+M.linter =  {
 	cmd = 'norminette',
 	stdin = false,
 	args = {},
 	stream = 'stdout',
 	ignore_exitcode = true,
-	parser = norminette_parser,
+	parser = parser,
 }
 
-require('lint').linters_by_ft = {
-	c = { 'norme', },
-}
-
-vim.cmd "autocmd BufNew *.c lua require('lint').try_lint()"
-vim.cmd "autocmd InsertLeave *.c lua require('lint').try_lint()"
-vim.cmd "autocmd BufWritePost *.c lua require('lint').try_lint()"
+return M
