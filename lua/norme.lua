394a3704c6e8f79cef233112f9f78f8848105641
Vinícius Gonçalves de Oliveira <viniciusp.olivera@gmail.com>
2021-05-15 16:45:44 -0300

    Removed log line in norme.lua

--- lua/norme.lua
+++ lua/norme.lua
@@ -6,7 +6,6 @@ local diagnostic_skeleton = { source = 'norminette', severity = ERROR }
 local stdin_parser = '../utils/stdin-parser'
 
 local parser = function(output)
-	print(output)
 	local result = vim.fn.split(output, "\n")
 	local diagnostics = {}
 
