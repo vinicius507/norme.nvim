ffd4769d62d0507ad377830f8621f30fc0c849b2
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-07-15 16:07:25 -0300

    prepping for merge

--- lua/norme.lua
+++ lua/norme.lua
@@ -5,7 +5,7 @@ local M = {}
 
 M.on_attach = function (_, _)
 	if not ok then
-		print('[Norme.nvim] new version of norme.nvim uses null-ls instead of nvim-lint.')
+		print('[Norme.nvim] new version of norme.nvim uses null-ls instead of nvim-lint. Refer to the Setup section on the README.')
 		return
 	end
 
@@ -13,6 +13,7 @@ M.on_attach = function (_, _)
 		print('[Norme.nvim] norme.nvim requires norminette version 3.3.0 or higher.')
 		return
 	end
+
 	local cfile = require('norme.cfile')
 	local hfile = require('norme.hfile')
 
@@ -21,6 +22,10 @@ M.on_attach = function (_, _)
 		name = 'norminette-c',
 		sources = { cfile, hfile }
 	})
+end
+
+M.lint = function ()
+	print('[Norme.nvim] new version of norme.nvim uses null-ls instead of nvim-lint. Refer to the Setup section on the README.')
 end
 
 return M
