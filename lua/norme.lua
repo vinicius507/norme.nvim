2c3416d70667f5af92854b1b15e475c15c5bb0f2
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-07-15 15:10:40 -0300

    started new version

--- lua/norme.lua
+++ lua/norme.lua
@@ -1,32 +1,25 @@
-local M = {
-	parser = require('norme/parser').parser,
-	should_lint = require('norme/utils').should_lint,
-}
+local ok, null_ls = pcall(require, 'null-ls')
+local utils = require('norme.utils')
 
-local stdin_parser = '../utils/stdin-parser'
+local M = {}
 
--- HACK: Need a better idea to get contents of current buffer.
-local function script_path()
-	local str = debug.getinfo(2, "S").source:sub(2)
-	return str:match("(.*/)") or "."
-end
-
-M.linter =  {
-	cmd = script_path() .. stdin_parser,
-	stdin = true,
-	args = {},
-	stream = 'stdout',
-	ignore_exitcode = true,
-	parser = M.parser,
-}
-
-M.lint = function()
-	if M.should_lint() then
-		local filename = vim.fn.expand('%:t')
-		local lint = require('lint')
-		lint.linters.norme.args = { filename }
-		lint.try_lint()
+M.on_attach = function ()
+	if not ok then
+		print('[Norme.nvim] new version of norme.nvim uses null-ls instead of nvim-lint.')
+		return
 	end
+
+	if not utils.check_norminette() then
+		print('[Norme.nvim] norme.nvim requires norminette version 3.3.0 or higher.')
+		return
+	end
+	local cfile = require('norme.cfile')
+
+	if not utils.should_lint() then
+		return
+	end
+	null_ls.setup()
+	null_ls.register({ name = 'norminette', filetypes = { 'c', 'cpp' }, sources = { cfile } })
 end
 
 return M
--- lua/norme/utils.lua
+++ lua/norme/utils.lua
@@ -26,4 +26,19 @@ M.should_lint = function ()
 	return false
 end
 
+M.check_norminette = function ()
+	local job = require('plenary.job')
+	local result = job:new({
+		command = 'norminette',
+		args = { '--v' },
+	}):sync()
+
+	local version
+	if type(result) == 'table' and result[1]:match('norminette .*') then
+		version = result[1]:match('norminette (.*)')
+		return (version > '3.3.0')
+	end
+	return false
+end
+
 return M
