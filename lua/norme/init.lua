5b8c8ffeeb6259a87542e821f6f9cb07f51447a9
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-12-18 18:55:53 -0300

    feat: decided to giveup on setup function, using null-ls .with()

--- lua/norme/init.lua
+++ lua/norme/init.lua
@@ -1,11 +1,27 @@
 local norme = {}
 
-local config = require('norme.config')
+local h = require('null-ls.helpers')
+local DIAGNOSTICS = require('null-ls.methods').internal.DIAGNOSTICS
 
----Sets up Norme.nvim, if not called, the defaults will be used
----@param opts table: configuration for Norme.nvim
-function norme.setup(opts)
-	config.setup(opts)
-end
+local helpers = require('norme.helpers')
+
+norme.diagnostics = h.make_builtin({
+	method = DIAGNOSTICS,
+	filetypes = { 'c', 'cpp' },
+	generator_opts = {
+		command = 'norminette',
+		args = { '--stdin', '--filename', '$FILENAME' },
+		to_stdin = true,
+		from_stderr = true,
+		timeout = 200,
+		format = 'line',
+		on_output = h.diagnostics.from_pattern(
+			[[Error:%s+([%a+_?]+)%s+%(line:%s+(%d+),%s+col:%s+(%d+)%):%s+(.*)$]],
+			{ 'code', 'row', 'col', 'message' }
+		),
+		runtime_condition = helpers.check_header,
+	},
+	factory = h.generator_factory,
+})
 
 return norme
